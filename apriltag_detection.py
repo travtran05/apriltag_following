cb626a525322be13429f31becaa58520859bb8cb
travistran <travistran544@gmail.com>
2023-08-02 07:21:25 -0400

    tags work?

--- apriltag_detection.py
+++ apriltag_detection.py
@@ -10,27 +10,18 @@ def write_video(video):
     width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    output_file = 'output_apriltag_video.avi'
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    output_video = cv2.VideoWriter(output_file, fourcc, 30, (width, height))
+    #output_file = 'output_apriltag_video.avi'
+    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    #output_video = cv2.VideoWriter(output_file, fourcc, 30, (width, height))
 
     ret, frame = video.read()
-    i = 0
-    cameraMatrix = numpy.array([ 1060.71, 0, 960, 0, 1060.71, 540, 0, 0, 1]).reshape((3,3))
-    camera_params = ( cameraMatrix[0,0], cameraMatrix[1,1], cameraMatrix[0,2], cameraMatrix[1,2] )
-    at_detector = Detector(families='tag36h11',
-                        nthreads=1,
-                        quad_decimate=1.0,
-                        quad_sigma=0.0,
-                        refine_edges=1,
-                        decode_sharpening=0.25,
-                        debug=0)
     
     frames = []
     frames.append(frame)
     while ret:
         ret, frame = video.read()
         frames.append(frame)
+    return frames
 
 def detect_tag(frame, at_detector, cameraMatrix = numpy.array([ 1060.71, 0, 960, 0, 1060.71, 540, 0, 0, 1]).reshape((3,3))):
 
@@ -38,7 +29,7 @@ def detect_tag(frame, at_detector, cameraMatrix = numpy.array([ 1060.71, 0, 960,
 
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    shape = img.shape()
+    shape = img.shape
     tags = at_detector.detect(img, True, camera_params, tag_size = 0.1)
     for tag in tags:
         pos = tag.center
@@ -57,26 +48,31 @@ def PID_tags(frameShape, horizontal_distance, vertical_distance, horizontal_pid,
     return horizontal_output, vertical_output
     
 
-def DrawOnImage(img, tagPositions, horizontalPid, verticalPid):
+def drawOnImage(img, tagPositions, horizontalPidOutput, verticalPidOutput):
     """
     Takes in the img, tagPositions as a 2d array, with each tag having a vertical, horizizontal cord, and a depth. 
     Also takes in the horizontalPid and verticalPid outputs
     """
-    for tag in tagPositions:
-        #draw the center of the tag
-        imgWidth =  img.shape[0]
-        imgHeight =  img.shape[1]
-        widthCenter = int(imgWidth/2)
-        heightCenter = int(imgHeight/2)
-        xCord = tag[0]
-        yCord = tag[1]
-        cv2.circle(img, (xCord,yCord), 10, (0, 0, 255), 5)
-        #draw where the center of the tag lies on the x an y axis
-        cv2.circle(img, (xCord,int(imgWidth/2)), 10, (0, 0, 255), 5)
-        cv2.circle(img, (yCord,int(imgHeight/2)), 10, (0, 0, 255), 5)
+    #draw the center of the tag
+    imgWidth =  img.shape[0]
+    imgHeight =  img.shape[1]
+    widthCenter = int(imgWidth/2)
+    heightCenter = int(imgHeight/2)
+    xCord = tagPositions[0]
+    yCord = tagPositions[1]
+    cv2.circle(img, (int(xCord),int(yCord)), 50, (255, 0, 0), 5)
+    #draw where the center of the tag lies on the x an y axis
+    cv2.circle(img, (int(imgWidth/2),int(yCord)), 50, (255, 0, 0), 5)
+    cv2.circle(img, (int(xCord),int(imgHeight/2)), 50, (255, 0, 0), 5)
     #draw the PID vectors as arrows
-    cv2.arrowedLine(img, (widthCenter,heightCenter), (widthCenter, int(verticalPid) + heightCenter), 
+    cv2.arrowedLine(img, (widthCenter,heightCenter), (widthCenter, int(verticalPidOutput) + heightCenter), 
             (0, 100, 255), 5, tipLength = 0.5)
-    cv2.arrowedLine(img, (widthCenter,heightCenter), (int(horizontalPid) + widthCenter,heightCenter), 
+    cv2.arrowedLine(img, (widthCenter,heightCenter), (int(horizontalPidOutput) + widthCenter,heightCenter), 
             (0, 100, 255), 5, tipLength = 0.5)
+    
+    cv2.putText(img, str(horizontalPidOutput), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(img, str(verticalPidOutput), (300, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    #img_array.append(frame)
+    print(f"Horizontal PID Output: {horizontalPidOutput}%")
+    print(f"Vertical PID Output: {verticalPidOutput}%")
     return img
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def write_video(video):
     output_file = 'output_apriltag_video.avi'
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     output_video = cv2.VideoWriter(output_file, fourcc, 30, (width, height))
-    
+
 
 
 # Create the video object
@@ -48,37 +48,8 @@ def _get_frame():
     vertical_pid = PID(1, 0, 0, 100)
     horizontal_pid = PID(1, 0, 0, 100)
     try:
-        while True:
-            if video.frame_available():
-                frame = video.frame()
-                # TODO: Add frame processing here
-                img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-                tags = at_detector.detect(img, False, camera_params, tag_size = 0.1)
-                for tag in tags:
-                    for idx in range(len(tag.corners)):
-                        cv2.line(color_img, tuple(tag.corners[idx - 1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
-                        cv2.circle(color_img, (int(tag.center[0].item()),int(tag.center[1].item())), 50, (0, 0, 255), 2)
-                    
-                    pos = tag.center
-                    #print(type(tag.center[0]))
-                    #cv2.line(color_img,,(960,540),(0,255,0),5)
-                    cv2.arrowedLine(color_img,(960,540),(-1*(int(960+pos[0])),540),(255,0,0),5)
-                    cv2.arrowedLine(color_img,(960,540),(960,-1*(int(540+pos[1]))),(255,0,0),5)
-                    horizontal_output, vertical_output = PID_tags(frame.shape, pos[0], pos[1], horizontal_pid, vertical_pid)
-                    #print(frame.shape)
-                    print(horizontal_output, vertical_output)
-                    #cv2.arrowedLine(color_img,(960,540),(960,int(vertical_output)+540),(255,0,0),5)
-                    #cv2.arrowedLine(color_img,(960,540),(int(horizontal_output)+960,540),(255,0,0),5)
-                    cv2.putText(color_img, str(horizontal_output), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                    cv2.putText(color_img, str(vertical_output), (300, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-                    #img_array.append(frame)
-                    print(f"Horizontal PID Output: {horizontal_output}%")
-                    print(f"Vertical PID Output: {vertical_output}%")
-                # TODO: set vertical_power and lateral_power here
-                lateral_power = horizontal_output
-                vertical_power = vertical_output
-                print(frame.shape)
+        # TODO: set vertical_power and lateral_power here
+        pass
     except KeyboardInterrupt:
         return
 
